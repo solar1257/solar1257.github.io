ea65b162f8132663345952c1f1016d3bb9d90325
정대현 <81551265+solar1257@users.noreply.github.com>
2025-05-10 16:46:22 +0900

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Saad Pasta",
-  title: "안녕~~",
+  title: "제발 ㅠㅠ~~",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
